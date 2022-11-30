98d55150262ba10893f7bebe42f18e77676872c9
JackCokebb <reo2726@gmail.com>
2022-11-30 21:39:02 +0900

    feat: detail page load && img file name fix

--- src/js/detail_load.js
+++ src/js/detail_load.js
@@ -63,3 +63,74 @@ const getCongestionStat = async (poi_Id) => {
     throw new Error("Failed to fetch at `getCongestionStat`");
   }
 }
+
+//실시간 혼잡도 게이지바 설정
+const setRealtimeGauge = async (poiId) =>{
+  const realtimeCongestion = await getCongestion(poiId);
+  console.log(realtimeCongestion)
+
+  const element = document.getElementById("realtime-gauge");
+  const congestion = realtimeCongestion['contents']['rltm']['congestionLevel'];
+  element.style.width = `${congestion}0%`;
+  element.ariaValueNow = `${congestion}`;
+  element.innerText = `${congestion}`;
+}
+
+//어제 기준 혼잡도 게이지바 설정
+//API 호출 시간 기준
+const setTimelyGauge = async (poiId) =>{
+  const today = new Date();
+  const currHour = today.getHours();//0~23
+
+  const timelyCongestion = await getTimelyCongestion(poiId, "ystday");
+  console.log(timelyCongestion)
+
+  const element = document.getElementById("timely-gauge");
+  const congestion = timelyCongestion['contents']['raw'][currHour]['congestionLevel'];
+  element.style.width = `${congestion}0%`;
+  element.ariaValueNow = `${congestion}`;
+  element.innerText = `${congestion}`;
+}
+
+//통계성 혼잡도 게이지바 설정
+//이전 한달 기준 해당 요일 해당 시간의 평균 혼잡도
+const setStatGauge = async (poiId) =>{
+  const today = new Date();
+  const currHour = today.getHours();//0~23
+  let currDay = today.getDay();   //sunday ~ saturday : 0 ~ 6
+  currDay = currDay - 1 > -1 ? currDay - 1 : 6; //monday ~ sunday : 0 ~ 6
+  console.log(`currHour = ${currHour} && currDay = ${currDay}`);
+
+  const congestStat = await getCongestionStat(poiId);
+  console.log(congestStat)
+
+  const element = document.getElementById("stat-gauge");
+  const congestion = congestStat['contents']['stat'][currDay*24 + currHour]['congestionLevel'];
+  element.style.width = `${congestion}0%`;
+  element.ariaValueNow = `${congestion}`;
+  element.innerText = `${congestion}`;
+
+}
+
+//detail 페이지 장소 이미지 로드
+const setPlaceImage = async (poiId) => {
+  const imagePath = `../assets/places/${poiId}.jpg`;
+  document.getElementById('place-image').src = imagePath;
+}
+
+//when detail page loaded
+window.onload = () => {
+  const urlParams = new URL(location.href).searchParams;
+  const poiId = urlParams.get('poiId');
+  console.log(poiId);
+
+  setPlaceImage(poiId)
+
+  setRealtimeGauge(poiId);
+  setTimelyGauge(poiId);
+  setStatGauge(poiId);
+
+
+
+
+}
